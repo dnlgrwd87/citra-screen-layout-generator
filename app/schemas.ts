87551f81b6f3ca36fe3b6ab9df2efd22444ff403
dnlgrwd87 <dnlgrwd@gmail.com>
2024-07-04 20:03:53 -0400

    fix imports

--- app/schemas.ts
+++ app/schemas.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod';
-import { games, resolutions } from './constants';
+import { GAMES, RESOLUTIONS } from './constants';
 
 export const StateFromParamsSchema = z.object({
-    gameId: z.custom<string>((val) => Object.keys(games).includes(val)),
-    resolutionId: z.custom<string>((val) => Object.keys(resolutions).includes(val)),
+    gameId: z.custom<string>((val) => Object.keys(GAMES).includes(val)),
+    resolutionId: z.custom<string>((val) => Object.keys(RESOLUTIONS).includes(val)),
     topX: z.number(),
     topY: z.number(),
     topWidth: z.number(),
