37247ad723fb3d0a295b47479bd57ffa21e78707
dnlgrwd87 <dnlgrwd@gmail.com>
2024-07-06 16:31:46 -0400

    Style invalid page

--- app/components/InvalidLayout.tsx
+++ app/components/InvalidLayout.tsx
@@ -1,7 +1,25 @@
+'use client';
+
+import { Button } from '@mui/material';
+import { useRouter } from 'next/navigation';
+
 export default function InvalidLayout() {
+    const router = useRouter();
+
     return (
-        <div>
-            <h1>Could not find a layout with the provided ID.</h1>
+        <div className="text-center py-8">
+            <h1 className="mb-4 text-5xl font-semibold text-primary-600">Uh-oh!</h1>
+            <p className="mb-10 text-3xl font-bold text-gray-900">
+                We can't find that custom layout.
+            </p>
+            <Button
+                onClick={() => {
+                    router.push('/');
+                    router.refresh();
+                }}
+            >
+                Create new layout
+            </Button>
         </div>
     );
 }
